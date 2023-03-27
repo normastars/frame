bf9aaf0d03545894dc42062e7d5c37d9a446afd0
li.yu <yu.li@netstars.co.jp>
2023-03-27 22:28:24 +0800

    [fix] add config

--- demo/main.go
+++ demo/main.go
@@ -5,19 +5,22 @@ import (
 
 	"github.com/nomainc/frame"
 	"github.com/nomainc/frame/version"
-	"github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v3"
 )
 
 func main() {
 	fmt.Printf("commit: %20s\n", version.GitCommit)
 	fmt.Printf("built on %20s\n", version.BuildGoVersion)
 	fmt.Printf("built on %20s\n", version.BuildSystem)
-	router := frame.Default()
-	router.GET("/hello", func(c *frame.Context) {
-		logrus.Info("哈哈")
-		logrus.Infof("hello world %s", "v")
-		c.Success(nil)
-	})
+	// router := frame.Default()
+	// router.GET("/hello", func(c *frame.Context) {
+	// 	logrus.Info("哈哈")
+	// 	logrus.Infof("hello world %s", "v")
+	// 	c.Success(nil)
+	// })
 
-	router.Run(":8080")
+	// router.Run(":8080")
+	config := frame.ReadConfig()
+	b, _ := yaml.Marshal(config)
+	fmt.Println(string(b))
 }
